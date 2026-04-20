04d42d0444cd376db8db7132db8669af3480c3b0
Dominic Couture <dominic@clerk.dev>
2026-04-20 09:25:24 +0100

    Format script

--- scripts.sh
+++ scripts.sh
@@ -1,2 +1,8 @@
-jq -r 'select( .lastDeploymentDate < "2025-01-01T00:00:00Z" ) | "- [`" + .name + "`](https://vercel.com/clerk-production/" + .name + ")\n  - url: https://" + .productionTargetAliases[0] + "\n  - repo: " + .repo.url + "\n  - last deployment: " + .lastDeploymentDate' output.ndjson
+echo "Projects that we not deployed in the last year"
+jq -r 'select( .lastDeploymentDate < "'$(date -v-1y "+%Y-%m-%dT%H:%M:%SZ")'" ) | "- [`" + .name + "`](https://vercel.com/clerk-production/" + .name + ")\n  - url: https://" + .productionTargetAliases[0] + "\n  - repo: " + .repo.url + "\n  - last deployment: " + .lastDeploymentDate' output.ndjson
+
+echo "Projects that were never deployed"
+jq -r 'select( .lastDeploymentDate == null ) | "- [`" + .name + "`](https://vercel.com/clerk-production/" + .name + ")\n  - url: https://" + .productionTargetAliases[0] + "\n  - repo: " + .repo.url' output.ndjson
+
+echo "Projects that are archived"
 jq -r 'select( .repo.isArchived ) | "- [`" + .name + "`](https://vercel.com/clerk-production/" + .name + ")\n  - url: https://" + .productionTargetAliases[0] + "\n  - repo: " + .repo.url + "\n  - last deployment: " + .lastDeploymentDate' output.ndjson
